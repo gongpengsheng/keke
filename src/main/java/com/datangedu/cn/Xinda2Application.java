4eeaf6c475039b7c8d43292e4add149c0dfffda0
Z1404719929 <1404719929@qq.com>
2020-06-01 19:05:14 +0800

    添加有关网页跳转的代码

--- src/main/java/com/datangedu/cn/Xinda2Application.java
+++ src/main/java/com/datangedu/cn/Xinda2Application.java
@@ -2,10 +2,12 @@ package com.datangedu.cn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
 public class Xinda2Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Xinda2Application.class, args);
 	}
